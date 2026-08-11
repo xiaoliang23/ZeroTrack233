a0207232bd81fd53f0a6582f66007890edbdf2b2
xiaoliang <66178220+xiaoliang23@users.noreply.github.com>
2026-08-11 22:17:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/ZeroTrack233/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
